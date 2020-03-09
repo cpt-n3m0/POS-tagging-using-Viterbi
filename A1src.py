b429dd1a34e6088542c6919c9e5c2c0a03d7cc96
faj5 <farouqjouti@gmail.com>
2020-03-09 13:27:33 +0000

    prepretty final

--- A1src.py
+++ A1src.py
@@ -2,59 +2,20 @@ from nltk.corpus import brown
 from nltk import FreqDist, WittenBellProbDist, bigrams
 import sys
 import re
+import time
+
+
 TRAIN = 10000
 TEST = 500
 RARETHRESH = 1
+PREPROCESS = True
+PP_CAP = True
+PP_SUF = True
+suffixes = ['ing', 'able', 'ible', 'ed', 'ly', 'dom', 'ism', 'ness', 'ist', 'ship', 'al', 'ance', 'tion', 'sion', 'ise']
 
-sents = brown.tagged_sents(tagset='universal')
-train_sents= sents[:TRAIN]
-test_sents = sents[TRAIN:TRAIN + TEST]
-
-# initial counts for preprocessing use
-words = [w for s in train_sents for (w, _) in s]
-wc = FreqDist(words)
+VERBOSE = False
 
 
-tags = []
-total_bigrams = []
-wt_pairs = []
-nwords= [] #processed words
-
-for sentence in train_sents:
-    sentence = [('S', 'S')] + sentence + [('E', 'E')]
-    stags = []
-    for i in range(len(sentence)):
-        word = sentence[i][0]
-        #preprocess
-        suffixes = ['ing', 'able', 'ible', 'ed', 'ly', 'dom', 'ism', 'ness', 'ist', 'ship', 'al', 'ance', 'tion', 'sion', 'ise']
-        if wc[word] <= RARETHRESH :
-            if len(word) > 1 and word[0].isupper() and i > 1:
-             #   print(" {} {} {}".format(sentence[i-1][0], sentence[i][0], sentence[i + 1][0]))
-             #   print(i)
-                sentence[i]= ('N-UNK', sentence[i][1])
-            for suf in suffixes:
-                if word.endswith(suf):
-                    sentence[i]= ('UNK-' + suf, sentence[i][1])
- 
-
- 
- 
-        stags += [sentence[i][1]]
-        nwords += [sentence[i][0]]
-        wt_pairs += [sentence[i]]
-
-    
-    total_bigrams += list(bigrams(stags))
-    tags += stags
-words = nwords    
-tag_bi = FreqDist(total_bigrams)
-            
-
-wc = FreqDist(words)
-print(wc['UNK-ing'])
-tag_count = FreqDist(tags)
-ts = list(tag_count.keys())
-tn = len(tags)
 
 
 
@@ -66,55 +27,51 @@ def create_table(x, y):
             tab[i].append(0)
     return tab
 
-def build_trans(tagset):
-    global tag_bi, tag_count
+def build_trans(tagset, tbigrams):
+    #global tag_bi, tag_count
     smoothed = {}
     for tag in tagset:
-        transitions = [t for (o, t) in total_bigrams if o == tag]
+        transitions = [t for (o, t) in tbigrams if o == tag]
         smoothed[tag] = WittenBellProbDist(FreqDist(transitions), bins=1e5)
     
     return smoothed
 
-def build_emis(tagset):
-    global wt_count, tag_count, types, wt_pairs
+def build_emis(tagset, wrd_tag_pairs):
     smoothed = {}
     for tag in tagset:
-        ws = [w for (w, t) in wt_pairs if t == tag]
+        ws = [w for (w, t) in wrd_tag_pairs if t == tag]
         smoothed[tag] = WittenBellProbDist(FreqDist(ws), bins=1e5)
     
     return smoothed
 
 
-emis = build_emis(ts)
-trans = build_trans(ts)
-
-def viterbi(wseq, tagset=ts, ep = emis, tp = trans):
-    global init_probs  
+def viterbi(wseq, tagset, ep , tp ):
     vitable = create_table(len(tagset), len(wseq))
-   # backpoints = create_table(len(tagset), len(wseq))
-    tseq = len(wseq) * [0]
+    backpoints = create_table(len(tagset), len(wseq))
+
 
     for t in range(len(tagset)):
         vitable[t][0] = tp['S'].prob(tagset[t]) * ep[tagset[t]].prob(wseq[0])
-    #pretty_print(vitable)
+
     for i in range(1, len(wseq)):
         for q in range(len(tagset)):
             candidates = [(vitable[q_prime][i-1] * tp[tagset[q_prime]].prob(tagset[q]) * ep[tagset[q]].prob(wseq[i]), q_prime) for q_prime in range(len(tagset))]
             vitable[q][i] = max(candidates)[0]
-            #backpoints[q][i] = max(candidates)[1]
-            tseq[i - 1] = tagset[max(candidates)[1]]
-    #lastpoint = max([(vitable[q][len(wseq) - 1], q) for q in range(len(tagset))])[1] 
-    #bestPOS = len(wseq) * [0]
-    #bestPOS[-1] = tagset[lastpoint]
-   # for i in range(len(wseq) - 1, 0, -1):
-    #    pt = backpoints[lastpoint][i]
-    #    bestPOS[i - 1] = tagset[pt]
-    #    lastpoint=pt
-    tseq[-1] == tagset[max([(vittable[q][len(wseq) - 1], q) for q in range(len(tagset))])[1]]
-    #bestPOS[0] = 'S'
-    #return bestPOS
-    return tseq
-def eval(tst_s, tagset=ts):
+            backpoints[q][i] = max(candidates)[1]
+    
+    # trace back steps
+    lastpoint = max([(vitable[q][len(wseq) - 1], q) for q in range(len(tagset))])[1] 
+    bestPOS = len(wseq) * [0]
+    bestPOS[-1] = tagset[lastpoint]
+    for i in range(len(wseq) - 1, 0, -1):
+        pt = backpoints[lastpoint][i]
+        bestPOS[i - 1] = tagset[pt]
+        lastpoint=pt
+    bestPOS[0] = 'S'
+
+    return bestPOS
+
+def eval(tst_s, tagset, ep , tp):
     correct = 0
     count = 0
     confusion_matrix = create_table(len(ts), len(ts))
@@ -133,10 +90,8 @@ def eval(tst_s, tagset=ts):
         ws = [w for (w, _) in s]
         tgs = [t for (_, t) in s]
         
-        ptags = viterbi(ws)
-        print(tgs)
-        print(ptags)
-        for i in range( len(tgs)):
+        ptags = viterbi(ws, tagset, ep, tp)
+        for i in range(len(tgs)):
             count += 1
             if tgs[i] == ptags[i]:
                 correct +=1
@@ -154,13 +109,88 @@ def eval(tst_s, tagset=ts):
 
 
 
-eval(test_sents)
+def train(train_sents):
+    if PREPROCESS:
+        # initial counts for preprocessing use
+        words = [w for s in train_sents for (w, _) in s]
+        wc = FreqDist(words)
+
+    if VERBOSE: print("Training Start... ")
+    train_duration = time.time()
+    pp_duration = 0
+ 
+    tags = []
+    total_bigrams = []
+    wt_pairs = []
+    nwords= [] #processed words
+
+    counter = 0
+    for sentence in train_sents:
+        sentence = [('S', 'S')] + sentence + [('E', 'E')]
+        stags = []
+        print("Training progress: %d%% \r" % (counter * 100/len(train_sents)), end='', flush=True))
+        counter+= 1
+        for i in range(len(sentence)):
+            if PREPROCESS:
+                start = time.time()
+                word = sentence[i][0]
+                #preprocess
+                if wc[word] <= RARETHRESH :
+                    if len(word) > 1 and word[0].isupper() and i > 1:
+                        sentence[i]= ('N-UNK', sentence[i][1])
+                    for suf in suffixes:
+                        if word.endswith(suf):
+                            sentence[i]= ('UNK-' + suf, sentence[i][1])
+                pp_duration += time.time() - start
+            stags += [sentence[i][1]]
+            nwords += [sentence[i][0]]
+            wt_pairs += [sentence[i]]
+
+
+        total_bigrams += list(bigrams(stags))
+        tags += stags
+    words = nwords    
+    wc = FreqDist(words)
+
+    tag_bi_count = FreqDist(total_bigrams)            
+    tag_count = FreqDist(tags)
+    ts = list(tag_count.keys())
+
+    if VERBOSE:
+        print("Training complete.")
+        # tag stats
+        print("Tags occurences :")
+        for t in tag_count:
+            print("{} {}".format(t, str(tag_count[t])))
+        
+    train_duration = time.time() - train_duration
+    return wt_pairs, words, wc, tags, total_bigrams, tag_bi_count, ts
 
 
 
+def setFlags(cmd):
+    if "-pp" in cmd:
+        PREPROCESS = False
+    if '-cpp' in cmd:
+        PP_CAP = False
+    if '-sufpp':
+        PP_SUF = False
+    if '-v':
+        VERBOSE = True
 
+if __name__ == '__main__':
+    setFlags(sys.argv[1:])
+    sents = brown.tagged_sents(tagset='universal')
+    train_sents= sents[:TRAIN]
+    test_sents = sents[TRAIN:TRAIN + TEST]
+    
+    wt_pairs, words, wc, tags, tbigrams, tag_bi_count, ts = train(train_sents)
+    
+    eps = build_emis(ts, wt_pairs)
+    tps = build_trans(ts, tbigrams)
+    
+    eval(test_sents, ts, eps, tps)
 
-#viterbi(sys.argv[1].split(" "), ts)
 
     
 
