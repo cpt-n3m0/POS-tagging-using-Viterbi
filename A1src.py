fc4998c885bb74fe7d6fb9520177a71a96c6c6bf
faj5 <farouqjouti@gmail.com>
2020-03-10 03:51:23 +0000

    almost there

--- A1src.py
+++ A1src.py
@@ -3,6 +3,7 @@ from nltk import FreqDist, WittenBellProbDist, bigrams
 import sys
 import re
 import time
+import matplotlib.pyplot as plt
 
 
 TRAIN = 10000
@@ -27,8 +28,33 @@ def create_table(x, y):
             tab[i].append(0)
     return tab
 
+#############
+def build_trans_nosmooth(tagset, tag_bigrams, tags):
+    trans = create_table(len(tagset), len(tagset))
+    tbcount = FreqDist(tag_bigrams)
+    tag_count = FreqDist(tags)
+    for i in range(len(tagset)):
+        for j in range(len(tagset)):
+            trans[i][j] = tbcount[(types[i], tagset[j])]/tag_count[tagset[i]]
+
+    return trans
+
+
+def build_emis_nosmooth(tagset, wrd_tag_pairs, types, tags):
+    emis = create_table(len(tagset), len(types))
+    
+    tag_count = FreqDist(tags)
+    wt_count = FreqDist(wrd_tag_pairs)
+    for i in range(len(tagset)):
+        for j in range(len(types)):
+            emis[i][j] = wt_count[(types[j], tagset[i])]/tag_count[tagset[i]]
+
+    return emis
+#############
+
+
+
 def build_trans(tagset, tbigrams):
-    #global tag_bi, tag_count
     smoothed = {}
     for tag in tagset:
         transitions = [t for (o, t) in tbigrams if o == tag]
@@ -45,13 +71,14 @@ def build_emis(tagset, wrd_tag_pairs):
     return smoothed
 
 
+
 def viterbi(wseq, tagset, ep , tp ):
     vitable = create_table(len(tagset), len(wseq))
     backpoints = create_table(len(tagset), len(wseq))
 
 
     for t in range(len(tagset)):
-        vitable[t][0] = tp['S'].prob(tagset[t]) * ep[tagset[t]].prob(wseq[0])
+        vitable[t][0] = tp['<s>'].prob(tagset[t]) * ep[tagset[t]].prob(wseq[0])
 
     for i in range(1, len(wseq)):
         for q in range(len(tagset)):
@@ -67,7 +94,7 @@ def viterbi(wseq, tagset, ep , tp ):
         pt = backpoints[lastpoint][i]
         bestPOS[i - 1] = tagset[pt]
         lastpoint=pt
-    bestPOS[0] = 'S'
+    bestPOS[0] = '<s>'
 
     return bestPOS
 
@@ -89,7 +116,7 @@ def train(train_sents):
 
     counter = 0
     for sentence in train_sents:
-        sentence = [('S', 'S')] + sentence + [('E', 'E')]
+        sentence = [('<s>', '<s>')] + sentence + [('</s>', '</s>')]
         stags = []
         print("Training progress: %d%% \r" % (counter * 100/len(train_sents) + 1), end='', flush=True)
         counter+= 1
@@ -97,7 +124,7 @@ def train(train_sents):
             if PREPROCESS:
                 start = time.time()
                 word = sentence[i][0]
-                #preprocess
+                # --- start preprocessing
                 if wc[word] <= RARETHRESH :
                     if PP_CAP:
                         if len(word) > 1 and word[0].isupper() and i > 1:
@@ -107,6 +134,8 @@ def train(train_sents):
                             if word.endswith(suf):
                                 sentence[i]= ('UNK-' + suf, sentence[i][1])
                 pp_duration += time.time() - start
+                # ----
+
             stags += [sentence[i][1]]
             nwords += [sentence[i][0]]
             wt_pairs += [sentence[i]]
@@ -116,8 +145,8 @@ def train(train_sents):
         tags += stags
     words = nwords    
     wc = FreqDist(words)
+    types = list(wc.keys())
     print("")
-    tag_bi_count = FreqDist(total_bigrams)            
     tag_count = FreqDist(tags)
     ts = list(tag_count.keys())
 
@@ -128,16 +157,32 @@ def train(train_sents):
         print("* Preprocessing lasted:\t{}ms ({}%)".format(str(pp_duration), str(int(pp_duration * 100/train_duration))))
         print("* Tags occurences :")
         for t in tag_count:
-            if t in ['S', 'E']:
+            if t in ['<s>', '</s>']:
                 continue
             print("{}\t{}".format(t, str(tag_count[t])))
         print("* Type count :\t" + str(type_count))
         print("* Token Count:\t" + str(len(words)))
+        # graph generation
+        # tag distribution
+        #plt.barh(list(tag_count.keys()), list(tag_count.values()))
+        #plt.xlabel("Number of Occurences")
+        #plt.ylabel("Tags")
+        #plt.title("Distribution of tags in the training data")
+       # plt.show()
+        # word frequency distribution
         
-    return wt_pairs, words, wc, tags, total_bigrams, tag_bi_count, ts
+        #print(sorted(list(wc.keys()), key= lambda key: wc[key], reverse=True)[:9])
+        #plt.hist(x=list(wc.values()), bins='auto', log=True)
+        #plt.ylabel("Frequency")
+        #plt.xlabel("Number of word occurences")
+        #plt.title("Word frequency distribution")
+        #plt.savefig("wordfreq.png", dpi=400)
+        
+        
+    return wt_pairs, words, types, wc, tags, total_bigrams, ts
 
 def eval(tst_s, tagset, ep , tp, wds):
-    if VERBOSE: print("\nStaring Testing ...")
+    if VERBOSE: print("\nStarting Testing ...")
     correct = 0
     count = 0
     confusion_matrix = create_table(len(ts), len(ts))
@@ -148,7 +193,7 @@ def eval(tst_s, tagset, ep , tp, wds):
         
         print("Testing progress: %d%% \r" % (counter * 100/len(tst_s) + 1), end='', flush=True)
         counter += 1
-        s =[('S', 'S')]+ s + [('E', 'E')]
+        s =[('<s>', '<s>')]+ s + [('</s>', '</s>')]
         
         for i in range(len(s)):
             w = s[i][0]
@@ -181,18 +226,19 @@ def eval(tst_s, tagset, ep , tp, wds):
         ntwords= [w for s in train_sents for (w, _) in s]
         ntwc = FreqDist(ntwords)
         print("* Test type count:\t" + str(len(ntwc.keys())))
+        print("* Test token count:\t" + str(len(ntwords)))
         print("* UNK occurences (% of total words)")
         for unk in unks:
             print("{}\t{} ({}%)".format(unk, str(unks[unk]), str(unks[unk] * 100/len(ntwords))))
         print("")
 
-    print("-" * 48 + "Confusion Matrix" + '-' * 48)
-    print("a\p\t" + "\t".join(ts))
-    for i in range(len(confusion_matrix)):
-        line = ts[i] + "\t"
-        for j in range(len(confusion_matrix)):
-                line += str(confusion_matrix[i][j])+ "\t"
-        print(line)
+    #print("-" * 48 + "Confusion Matrix" + '-' * 48)
+    #print("a\p\t" + "\t".join(ts))
+    #for i in range(len(confusion_matrix)):
+    #    line = ts[i] + "\t"
+    #    for j in range(len(confusion_matrix)):
+    #            line += str(confusion_matrix[i][j])+ "\t"
+    #    print(line)
     print("Accuracy rate is :" + str((correct * 100)/count))
 
 
@@ -215,13 +261,53 @@ if __name__ == '__main__':
     train_sents= sents[:TRAIN]
     test_sents = sents[TRAIN:TRAIN + TEST]
     
-    wt_pairs, wds, wc, tags, tbigrams, tag_bi_count, ts = train(train_sents)
+    allsuffixes = [ 'ing', 'able', 'ible', 'ed', 'ly', 'dom', 'ism', 'ness', 'ist', 'ship', 'al', 'ance', 'tion', 'sion', 'ise']
+    for s in allsuffixes:
+        suffixes=[s]
+        wt_pairs, wds, types, wc, tags, tbigrams, ts = train(train_sents)
+        eps = build_emis(ts, wt_pairs)
+        tps = build_trans(ts, tbigrams)
+        print("evaluating " + s)
+        eval(test_sents, ts, eps, tps, wds )
     
-    eps = build_emis(ts, wt_pairs)
-    tps = build_trans(ts, tbigrams)
-    
-    eval(test_sents, ts, eps, tps, wds )
 
+    if VERBOSE:
+        #ns_tps = build_trans_nosmooth(ts, tbigrams, tags)
+        #ns_eps = build_emis_nosmooth(ts, wt_pairs, types, tags)
+        print("-" * 48 + "Transition Probs" + '-' * 48) 
+        print("\t" + "\t".join(ts))
+        for i in range(len(ts)):
+            line = " " + ts[i] + "\t"
+            for j in range(len(ts)):
+                    line += str("%.5f" % tps[ts[i]].prob(ts[j]))+ "\t"
+            print(line)
+        # Non smoothed transition probs
+
+        #print("-" * 48 + "No smoothing Transition Probs" + '-' * 48) 
+        #print("\t" + "\t".join(ts))
+        #for i in range(len(ts)):
+        #    line = " " + ts[i] + "\t"
+        #    for j in range(len(ts)):
+        #            line += str("%.5f" % ns_tps[i][j])+ "\t"
+        #    print(line)
+        print("-" * 30+ "Emission Probs" + '-' * 30)
+        print("\t" + "\t".join(wds[:9]))
+        for i in range(len(ts)):
+            line = " " + ts[i] + "\t"
+            for j in range(len(wds[:9])):
+                    line += str("%.5f" % eps[ts[i]].prob(wds[j]))+ "\t"
+            print(line)
+        #print("-" * 30 + "no smoothing Emission Probs" + '-' * 30)
+        #print("\t" + "\t".join(wds[:9]))
+        #for i in range(len(ts)):
+        #    line = " " + ts[i] + "\t"
+        #    for j in range(len(wds[:9])):
+        #            line += str("%.5f" % ns_eps[i][j])+ "\t"
+        #    print(line)
+
+
+    
+    
 
     
 
